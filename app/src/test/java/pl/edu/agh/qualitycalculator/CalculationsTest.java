07c61858b99d71464624da77de993fb972f94136
kutermak <kutermak@student.agh.edu.pl>
2018-01-13 15:26:18 +0100

    Dodane testy

--- app/src/test/java/pl/edu/agh/qualitycalculator/CalculationsTest.java
+++ app/src/test/java/pl/edu/agh/qualitycalculator/CalculationsTest.java
@@ -19,6 +19,18 @@ public class CalculationsTest {
         assertEquals(5, calculationsUnderTest.calculate(Operation.SUM, 2, 3), 0.01);
     }
 
+    @Test
+    public void testMult() throws Exception {
+        assertEquals(6, calculationsUnderTest.calculate(Operation.MULTIPLY, 2, 3), 0.01);
+    }
+    @Test
+    public void testDiv() throws Exception {
+        assertEquals(6, calculationsUnderTest.calculate(Operation.DIVIDE, 12, 2), 0.01);
+    }
+    @Test
+    public void testSub() throws Exception {
+        assertEquals(6, calculationsUnderTest.calculate(Operation.SUBTRACT, 12, 6), 0.01);
+    }
     @After
     public void tearDown() {
     }
